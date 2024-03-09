7e8435caf642aed968d51e7805a616b307121593
JoseZabaleta <josezabaletag@gmail.com>
2024-03-09 16:17:04 -0300

    Segundo commit

--- src/main/java/com/proyectos/crudfullstackangular/controller/ClienteController.java
+++ src/main/java/com/proyectos/crudfullstackangular/controller/ClienteController.java
@@ -2,10 +2,9 @@ package com.proyectos.crudfullstackangular.controller;
 
 import com.proyectos.crudfullstackangular.entity.Cliente;
 import com.proyectos.crudfullstackangular.service.ClienteService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ClienteController {
@@ -14,8 +13,14 @@ public class ClienteController {
     public ClienteController(ClienteService clienteService) {
         this.clienteService = clienteService;
     }
-    @PostMapping("")
+    @PostMapping("/api/create-cliente")
     public Cliente save(@RequestBody Cliente cliente){
         return clienteService.save(cliente);
     }
+
+@GetMapping
+    public List<Cliente> findAll(){
+        return clienteService.findAll();
+    }
+
 }
